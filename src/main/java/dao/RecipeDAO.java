2fe0fb40b6b293859026b7de0ff9073a79db3f64
snowone <snowone4426@gmail.com>
2022-10-07 20:24:07 +0900

    작업 위치 변경으로 인한 commit

--- src/main/java/dao/RecipeDAO.java
+++ src/main/java/dao/RecipeDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import util.DBConnPool;
 
@@ -40,8 +41,53 @@ public class RecipeDAO extends DBConnPool {
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+		  close();
 		}
 		
 		return theme_list;
+	}
+	
+	public List<RecipeObj> getHotRecipeList () {
+	  List<RecipeObj> hot_list = new ArrayList<RecipeObj>();
+	  String sql = "SELECT ROWNUM, recipe_id, title, subtitle, thumbnail FROM ("
+	      + "SELECT r.recipe_id, r.title, r.subtitle, r.thumbnail FROM recipe "
+	      + "INNER JOIN bookmark b ON r.recipe_id = n.recipe_id "
+	      + "GROUP BY r.recipe_id, r.title, r.subtitle, r.thumbnail ORDER BY count(*) DESC) "
+	      + "WHERE ROWNUM < 9";
+	  try {
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(sql);
+        
+        while (rs.next()) {
+          RecipeObj recipe = new RecipeObj();
+          
+          recipe.setRecipe_id(rs.getString("recipe_id"));
+          recipe.setTitle(rs.getString("title"));
+          recipe.setSubtitle(rs.getString("subtitle"));
+          recipe.setThumbnail(rs.getString("thumbnail"));
+          
+          hot_list.add(recipe);
+        }
+        
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        close();
+      }
+	  
+	  return hot_list;
+	}
+	
+	public List<RecipeObj> getRecipeList (Integer offset, Integer limit, Map<String,String> category) {
+	  List<RecipeObj> recipe_list = new ArrayList<RecipeObj>();
+	  String sql="SELECT ROWNUM, recipe_id, title, subtitle, thumbnail FROM ("
+	      + "SELECT r.recipe_id, r.title, r.subtitle, r.thumbnail FROM recipe "
+	      + "INNER JOIN recipecategory rc ON r.recipe_id = rc.recipe_id "
+	      + "INNER JOIN category c ON c.category_id = rc.category_id "
+	      + "WHERE c.name = '" + category + "' AND rc.sub_category = '" + sub_category + "' "
+	      + "ORDER BY r.created_at) WHERE ROWNUM BETWEEN 'offset * limit' AND '(offset + 1) * limit';";
+	  
+	  return recipe_list;
 	}
 }
