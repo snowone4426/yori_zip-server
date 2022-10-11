17695c598a3026338bc0564739e7a6b4e228c5c7
snowone <snowone4426@gmail.com>
2022-10-11 12:59:37 +0900

    feat: bannerDAO 완성
     - 메인배너 : 배너 정보 오브젝트가 들어있는 배열.

---
+++ src/main/java/dao/BannerDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.DBConnPool;
+
+public class BannerDAO extends DBConnPool {
+  public List<BannerObj> getBannerList () {
+    List<BannerObj> banner_list = new ArrayList<>();
+    String sql = "SELECT * FROM banner WHERE start_date < sysdate AND end_date > sysdate";
+    
+    try {
+      stmt = con.createStatement();
+      rs = stmt.executeQuery(sql);
+      
+      while(rs.next()) {
+        BannerObj banner = new BannerObj();
+        
+        banner.setBanner_id(rs.getString("banner_id"));
+        banner.setPhoto(rs.getString("photo"));
+        banner.setAlt(rs.getString("alt"));
+        banner.setCreated_at(rs.getDate("created_at"));
+        banner.setStart_date(rs.getDate("start_date"));
+        banner.setEnd_date(rs.getDate("end_date"));
+        banner.setSearch(rs.getString("search"));
+        
+        banner_list.add(banner);
+      }
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      close();
+    }
+    
+    return banner_list;
+  }
+}
